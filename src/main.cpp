5c00230736185b2cac39bff6fac2f687a294ec0a
Steve Grogorick <steve@grogorick.de>
2021-12-15 15:39:20 +0100

    cleanup / removed old example code

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,9 @@
 #include <vd2/VDXFrame/VideoFilter.h>
-#include <BlackWhiteFilter.h>
 #include <FrameAccuFilter.h>
 
-VDXFilterDefinition filterDef_blackWhite = VDXVideoFilterDefinition<BlackWhiteFilter>("Shadwork", "Black White filter", "Example for VirtualDub Plugin SDK: Applies a Black White filter to video.");
 VDXFilterDefinition filterDef_frameAccu = VDXVideoFilterDefinition<FrameAccuFilter>("Steve Grogorick", "Frame Accumulation filter", "Accumulate frame, e.g. for firework recordings.");
 
 VDX_DECLARE_VIDEOFILTERS_BEGIN()
-	VDX_DECLARE_VIDEOFILTER(filterDef_blackWhite)
 	VDX_DECLARE_VIDEOFILTER(filterDef_frameAccu)
 VDX_DECLARE_VIDEOFILTERS_END()
 
